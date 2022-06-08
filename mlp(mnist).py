5b81998f7a20337bffab46dd59f0d9af95a5ef94
rkskek1226 <hkcho01@naver.com>
2022-06-08 11:48:51 +0900

    Affine, Sigmoid, SoftmaxWithLoss

--- mlp(mnist).py
+++ mlp(mnist).py
@@ -42,3 +42,54 @@ def one_hot_encoding(x):
     return arr
 
 
+class Affine:
+    def __init__(self, w, b):
+        self.w = w
+        self.b = b
+        self.x = None
+        self.dw = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.w) + self.b
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.w.T)
+        self.dw = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        return dx
+
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy(self.y, self.t)
+        # self.loss = mse(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+        return dx
